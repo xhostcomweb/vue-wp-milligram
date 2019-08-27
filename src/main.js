3e60388f603dde42436b28278f8fdb74f7ea1b7a
xhostcom <support@xhostcom.com>
2019-08-27 11:40:53 +0100

    Update

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import './index.js';
-/*import '~/assets/css/style.css';*/
+import '~/assets/css/milligram.css';
 import DefaultLayout from '~/layouts/Default.vue'
 export default function (Vue,{ head, router, isServer } ) {
   Vue.component('Layout', DefaultLayout)
